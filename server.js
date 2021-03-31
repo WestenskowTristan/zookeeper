5bdbeb73f87e8563c9e8f63211353a049665bcd1
tristan <westenskowtristan122@gmail.com>
2021-03-31 17:02:03 -0600

    adding html and other front-end documents to project

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
 const fs = require("fs");
 const path = require("path");
-const { json } = require("body-parser");
+const { response } = require("express");
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -110,6 +111,22 @@ app.post("/api/animals", (req, res) => {
 
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
